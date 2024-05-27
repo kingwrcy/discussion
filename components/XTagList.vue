08357c682ee741847adf61c1922a2daf453b7706
wangrui06 <wangrui06@shengpay.com>
2024-05-27 18:17:44 +0800

    优化样式

--- components/XTagList.vue
+++ components/XTagList.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex text-sm">
-    <div class="flex space-x-2 flex-1">
-      <UBadge :color="selectedTag === tag.name ? 'primary' : 'gray'" variant="solid" size="md" class="cursor-pointer"
+    <div class="flex space-x-2 flex-1 overflow-auto">
+      <UBadge :color="selectedTag === tag.name ? 'primary' : 'gray'" variant="solid" size="md" class="shrink-0 cursor-pointer "
         v-for="tag in tagList">
         <NuxtLink :to="`/tag/${tag.name}`">{{ tag.name }}</NuxtLink>
       </UBadge>
       <div v-if="tagList.length === 0" class="text-sm">暂无标签,请去后台添加</div>
     </div>
-    <UBadge :color="selectedTag === 'all' ? 'primary' : 'gray'" variant="solid" size="md" class="cursor-pointer">
+    <UBadge :color="selectedTag === 'all' ? 'primary' : 'gray'" variant="solid" size="md" class="cursor-pointer w-[78px]" >
       <NuxtLink to="/tags">全部节点</NuxtLink>
     </UBadge>
   </div>
