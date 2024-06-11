eda7e1a926adb0d78a2a970307bd9a5a140b2d23
lateautumn02 <57248164+lateautumn2@users.noreply.github.com>
2024-06-11 10:32:23 +0800

    fix:修复headImg为空时提交报错

--- server/api/member/saveSettings.post.ts
+++ server/api/member/saveSettings.post.ts
@@ -22,7 +22,7 @@ export default defineEventHandler(async (event) => {
   const data = {
     email: request.email.trim(),
     avatarUrl: sha256(request.email.trim()),
-    headImg: request.headImg.trim(),
+    headImg: request.headImg?.trim(),
     css: request.css,
     js: request.js,
     signature: request.signature,
